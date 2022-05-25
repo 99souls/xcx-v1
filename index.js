3bd5fad4d417c0f55c8e2d76345d8aff274d77c1
andrew <andrew.l.foy@gmail.com>
2022-05-25 21:51:23 +0100

    added modular commands + support in index file

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // requiring necessary classes for discord.js
 
+const fs = require("node:fs");
+const path = require("node:path");
 const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
 // creating new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
 
 // once client is ready this code runs once
 // waits for 'ready' then following code
@@ -14,22 +28,19 @@ client.once("ready", () => {
   client.user.setPresence({ activities: [{ name: "with ur heart" }], status: "dnd" });
 });
 
-//     client.on("interactionCreate", async (interaction) => {
-//     if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-//     const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-//     if (commandName === "ping") {
-//         await interaction.reply("Pong!");
-//     } else if (commandName === "server") {
-//         await interaction.reply(
-//         `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nowner: <@${
-//             interaction.guild.ownerId
-//         }>\nicon url: ${interaction.guild.iconURL()}`
-//         );
-//     } else if (commandName === "user") {
-//         await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-//     }
-// });
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+  }
+});
 
 client.login(token);
