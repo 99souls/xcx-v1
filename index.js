255f2790364795ae3f746d699b36576f78d47cc9
andrew <andrew.l.foy@gmail.com>
2022-05-25 21:17:46 +0100

    reverted

--- index.js
+++ index.js
@@ -16,6 +16,3 @@ client.once("ready", () => {
 
 // log in to discord using client's token
 client.login(token);
-
-const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
-interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
