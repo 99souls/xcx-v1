d91d846419500d66fbd492a6cb8bcfe63a63bc9e
andrew <andrew.l.foy@gmail.com>
2022-05-25 20:56:33 +0100

    created base bot

---
+++ index.js
@@ -0,0 +1,16 @@
+// requiring necessary classes for discord.js
+
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
+
+// creating new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// once client is ready this code runs once
+// waits for 'ready' then following code
+client.once("ready", () => {
+  console.log("bot is ready");
+});
+
+// log in to discord using client's token
+client.login(token);
